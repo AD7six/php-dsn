8959c2c42a6d409263be5c909360a1c3d5a18fab
AD7six <andydawson76@gmail.com>
2014-01-20 19:06:04 +0000

    add some docs

--- src/DbDsn.php
+++ src/DbDsn.php
@@ -2,14 +2,40 @@
 
 namespace AD7six\Dsn;
 
+/**
+ * DbDsn
+ *
+ */
 class DbDsn extends Dsn {
 
+/**
+ * _defaultPort
+ *
+ * Overriden in subclasses
+ *
+ * @var int
+ */
 	protected $_defaultPort;
 
+/**
+ * _keyMap
+ *
+ * The path is the database name
+ *
+ * @var array
+ */
 	protected $_keyMap = [
 		'path' => 'database'
 	];
 
+/**
+ * keyMap
+ *
+ * make sure that the path is translated too, but not if it's already redefined
+ *
+ * @param mixed $keyMap
+ * @return mixed
+ */
 	public function keyMap($keyMap = null) {
 		if ($keyMap) {
 			$keyMap += ['path' => 'database'];
@@ -17,14 +43,32 @@ class DbDsn extends Dsn {
 		return parent::keyMap($keyMap);
 	}
 
+/**
+ * parseUrl
+ *
+ * The database (path) will have a leading slash - strip it off
+ *
+ * @param string $string
+ * @return array
+ */
 	public function parseUrl($string) {
 		$return = parent::parseUrl($string);
 
 		if (isset($this->_url['path'])) {
 			$this->_url['path'] = ltrim($this->_url['path'], '/');
 		}
+
+		return $return;
 	}
 
+/**
+ * _toUrl
+ *
+ * Re-prefix the database (path) with a leading slash
+ *
+ * @param mixed $data
+ * @return string
+ */
 	protected function _toUrl($data) {
 		if (isset($data['path'])) {
 			$data['path'] = '/' . $data['path'];
--- src/MysqlDsn.php
+++ src/MysqlDsn.php
@@ -2,7 +2,16 @@
 
 namespace AD7six\Dsn;
 
+/**
+ * MysqlDsn
+ *
+ */
 class MysqlDsn extends DbDsn {
 
+/**
+ * _defaultPort
+ *
+ * @var int 3306
+ */
 	protected $_defaultPort = 3306;
 }
