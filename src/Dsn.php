f4e617ae3a138a293df2dab2504cd95986429819
AD7six <andydawson76@gmail.com>
2014-01-20 17:11:11 +0000

    convert to instance methods, make bidirectional

--- src/Dsn.php
+++ src/Dsn.php
@@ -4,15 +4,23 @@ namespace AD7six\Dsn;
 
 class Dsn {
 
+	protected $_data = [];
+
+	public function __construct($string = '') {
+		$this->parseUrl($string);
+	}
+
 /**
  * parseUrl
  *
  * Parse a url and merge with any extra get arguments defined
  *
  * @param string $string
- * @return array
+ * @return void
  */
-	public static function parseUrl($string) {
+	public function parseUrl($string) {
+		$this->_data = [];
+
 		$url = parse_url($string);
 		if (!$url || array_keys($url) === ['path']) {
 			return false;
@@ -25,7 +33,56 @@ class Dsn {
 			$url += $extra;
 		}
 
-		return $url;
+		$this->_data = $url;
+	}
+
+	public function toArray() {
+		return $this->_data;
+	}
+
+	public function __get($name) {
+		if (array_key_exists($name, $this->_data)) {
+			return $this->_data[$name];
+		}
+
+		return null;
+	}
+
+	public function __toString() {
+		$urlKeys = [
+			'scheme' => '',
+			'host' => '',
+			'port' => '',
+			'user' => '',
+			'pass' => '',
+			'path' => ''
+		];
+
+		$url = array_intersect_key($this->_data, $urlKeys);
+
+		$return = $url['scheme'] . '://';
+		if (!empty($url['user'])) {
+			$return .= $url['user'];
+			if (!empty($url['pass'])) {
+				$return .= ':' . $url['pass'];
+			}
+			$return .= '@';
+		}
+		$return .= $url['host'];
+		if (!empty($url['port'])) {
+			$return .= ':' . $url['port'];
+		}
+		$return .= $url['path'];
+
+		$query = array_diff_key($this->_data, $urlKeys);
+		if ($query) {
+			foreach($query as $key => &$value) {
+				$value = "$key=$value";
+			}
+			$return .= '?' . implode($query, '&');
+		}
+
+		return $return;
 	}
 
 /**
@@ -35,7 +92,7 @@ class Dsn {
  * @param string $key
  * @return mixed
  */
-	protected static function _get($data, $key) {
+	protected function _get($data, $key) {
 		if (isset($data[$key])) {
 			return $data[$key];
 		}
@@ -50,7 +107,7 @@ class Dsn {
  * @param array $replacements
  * @return array
  */
-	protected static function _replace($data, $replacements) {
+	protected function _replace($data, $replacements) {
 		if (!$replacements) {
 			return $data;
 		}
@@ -58,7 +115,7 @@ class Dsn {
 		foreach($data as &$value) {
 			$value = str_replace(array_keys($replacements), array_values($replacements), $value);
 			if (is_array($value)) {
-				$value = static::_replace($value, $replacements);
+				$value = $this->_replace($value, $replacements);
 			}
 		}
 
--- tests/Dsn/DsnTest.php
+++ tests/Dsn/DsnTest.php
@@ -17,12 +17,16 @@ class DsnTest extends PHPUnit_Framework_TestCase {
  * @return void
  */
 	public function testParseUrl() {
+		$dsn = new Dsn('');
+
 		$expected = [
 			'scheme' => 'service',
 			'host' => 'host',
 			'path' => '/path'
 		];
-		$return = Dsn::parseUrl('service://host/path');
+		$dsn->parseUrl('service://host/path');
+		$return = $dsn->toArray();
+
 		$this->assertSame($expected, $return, 'a basic url should be parsed');
 
 		$expected = [
@@ -33,7 +37,8 @@ class DsnTest extends PHPUnit_Framework_TestCase {
 			'pass' => 'password',
 			'path' => '/database_name',
 		];
-		$return = Dsn::parseUrl('mysql://user:password@localhost:3306/database_name');
+		$dsn->parseUrl('mysql://user:password@localhost:3306/database_name');
+		$return = $dsn->toArray();
 		$this->assertSame($expected, $return, 'A url should be parsed into it\'s component parts');
 
 		$expected = [
@@ -46,8 +51,34 @@ class DsnTest extends PHPUnit_Framework_TestCase {
 			'encoding' => 'utf8',
 			'flags' => '0',
 		];
-		$return = Dsn::parseUrl('mysql://user:password@localhost:3306/database_name?encoding=utf8&flags=0');
+		$dsn->parseUrl('mysql://user:password@localhost:3306/database_name?encoding=utf8&flags=0');
+		$return = $dsn->toArray();
 		$this->assertSame($expected, $return, 'Option (get arguments) should be merged with the parsed url');
+	}
+
+/**
+ * testBidirectionalUrl
+ *
+ * @dataProvider bidirectionalProvider
+ * @param string $url
+ * @return void
+ */
+	public function testBidirectionalUrl($url)  {
+		$dsn = new Dsn($url);
+		$this->assertSame($url, (string) $dsn, 'The regenerated url should be identical to the input');
+	}
+
+/**
+ * bidirectionalProvider
+ *
+ * @return array
+ */
+	public function bidirectionalProvider() {
+		return [
+			['service://host/path'],
+			['mysql://user:password@localhost:3306/database_name'],
+			['mysql://user:password@localhost:3306/database_name?encoding=utf8&flags=0']
+		];
 	}
 
 }
