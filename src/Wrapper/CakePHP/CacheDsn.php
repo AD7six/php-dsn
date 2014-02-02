fb33e331a2968b60e78390e6f75aaa1651cf72c1
AD7six <andydawson76@gmail.com>
2014-02-02 15:56:49 +0000

    add a getter for correct case for engine

--- src/Wrapper/CakePHP/CacheDsn.php
+++ src/Wrapper/CakePHP/CacheDsn.php
@@ -10,21 +10,28 @@ class CacheDsn extends Dsn {
 		$keyMap = [
 			'scheme' => 'engine'
 		];
+		if (!isset($options['keyMap'])) {
+			$options['keyMap'] = [];
+		}
+		$options['keyMap'] += $keyMap;
+
+		$getters = [
+			'engine' => function($x, $dsn) {
+				return ucfirst($dsn->engine);
+			}
+		];
+		if (!isset($options['getters'])) {
+			$options['getters'] = [];
+		}
+		$options['getters'] += $getters;
 
 		$replacements = [
 			'/CACHE/' => CACHE,
 			'DURATION' =>  \Configure::read('debug') ? '+10 seconds' : '+999 days'
 		];
-
-		if (!isset($options['keyMap'])) {
-			$options['keyMap'] = [];
-		}
-
 		if (!isset($options['replacements'])) {
 			$options['replacements'] = [];
 		}
-
-		$options['keyMap'] += $keyMap;
 		$options['replacements'] += $replacements;
 
 		return (new Dsn($url, $options))->toArray();
