27301d84a20fcd58dc44715b5300a3bb3c035022
AD7six <andydawson76@gmail.com>
2014-02-03 11:04:34 +0000

    add email wrapper

---
+++ src/Wrapper/CakePHP/EmailDsn.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AD7six\Dsn\Wrapper\CakePHP;
+
+use AD7six\Dsn\Wrapper\Dsn;
+
+class EmailDsn extends Dsn {
+
+	protected $_defaultOptions = [
+		'keyMap' => [
+			'scheme' => 'transport',
+		]
+	];
+
+	public static function parse($url, $options = []) {
+		$inst = new EmailDsn($url, $options);
+		return $inst->toArray();
+	}
+
+	public function getTransport() {
+		$adapter = $this->_dsn->adapter;
+
+		if ($adapter) {
+			return $adapter;
+		}
+		return ucfirst($this->_dsn->scheme);
+	}
+
+	public function setTransport($value) {
+		$this->_dsn->scheme = lcfirst($value);
+	}
+
+}
