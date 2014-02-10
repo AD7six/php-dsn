c33c478858a8ba6ac6b8f5899fa87b6bc8993e67
AD7six <andydawson76@gmail.com>
2014-02-10 16:42:08 +0000

    correct path to composer when a dependency
    composer will install this repo as vendor/ad7six/dsn

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ date_default_timezone_set('UTC');
  * Path trickery ensures test suite will always run, standalone or within
  * another composer package. Designed to find composer autoloader and require
  */
-$vendorPos = strpos(__DIR__, 'vendor/AD7six/dsn');
+$vendorPos = strpos(__DIR__, 'vendor/ad7six/dsn');
 if ($vendorPos !== false) {
     // Package has been cloned within another composer package, resolve path to autoloader
     $vendorDir = substr(__DIR__, 0, $vendorPos) . 'vendor/';
